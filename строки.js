0bdc84cfbf50110632aecaa4f2916fcdbe03f057
SenchaKirill <kiryasen7490@gmail.com>
2023-11-13 18:41:40 +0300

    add строки

---
+++ строки.js
@@ -0,0 +1,37 @@
+/*
+const name1 = 'kirill';
+const name2 = "lera";
+
+console.log(name1, name2);
+
+
+const name = 'kirill';
+const prof = 'barmen';
+
+const allinfo = `${name} ${prof}`;
+
+console.log('allinfo', allinfo);
+*/
+
+/*
+//slice - обрезание текста
+const обрезаниетекста = "этот текст";
+console.log('обрезание текста', обрезаниетекста.slice(1, 4)); 
+
+//replace or riplaceall -замена символов в строке 
+const замена = "этот текст";
+console.log('замена', замена.replace('этот', '')); 
+*/
+
+//ДЗ
+
+const name1 = 'всем привет! меня зовут кирилл';
+const programmingLanguage = 'я изучаю js';
+const courseCreatorName = 'курс нормального типа';
+const reasconstonText = 'надоело все';
+const allinfo = `${name1} ${programmingLanguage} ${courseCreatorName} ${reasconstonText}`;
+console.log(allinfo);
+
+console.log(name1.replaceAll('в'), ('В'));
+
+
